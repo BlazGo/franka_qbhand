754d22a77f0ae38ff0cb2539c5aba7f030ae6db5
BlazGo <bg2286@student.uni-lj.si>
2023-08-03 17:25:15 +0200

    impedance change via dynamic reconfigure

--- src/franka_cartesian_impedance_param.py
+++ src/franka_cartesian_impedance_param.py
@@ -1,16 +1,14 @@
 import rospy
-from franka_msgs.srv import SetCartesianImpedance, SetCartesianImpedanceRequest
+from dynamic_reconfigure.client import Client
 
 class SetCartesianImpedanceParams:
     def __init__(self, node_name:str="set_cartesian_impedance") -> None:
         rospy.init_node(node_name)
-    
-    def set_params(self,
-                   name:str="/cartesian_impedance_example_controller/dynamic_reconfigure_compliance_param_node/translational_stiffness",
-                   value:float=400.0):
-        rospy.set_param(param_name=name,
-                        param_value=value)
+        self.client = Client("/cartesian_impedance_example_controller/dynamic_reconfigure_compliance_param_node", timeout=1, config_callback=self.callback)
+
+    def set_param(self):
+        self.client.update_configuration({"translational_stiffness": 100.0})
 
 if __name__ == "__main__":
     set_load = SetCartesianImpedanceParams()
-    set_load.set_params()
+    set_load.set_param()
