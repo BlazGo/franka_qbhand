a65e73559d5e3e6356b93f12991bef09bdf01e39
BlazGo <bg2286@student.uni-lj.si>
2023-08-20 09:49:08 +0200

    eul2quat (not needed)

--- src/tools.py
+++ src/tools.py
@@ -1,19 +1,19 @@
 import numpy as np
-import math as m
+import math
   
 def Rx(theta):
   return np.matrix([[ 1, 0           , 0           ],
-                    [ 0, m.cos(theta),-m.sin(theta)],
-                    [ 0, m.sin(theta), m.cos(theta)]])
+                    [ 0, math.cos(theta),-math.sin(theta)],
+                    [ 0, math.sin(theta), math.cos(theta)]])
   
 def Ry(theta):
-  return np.matrix([[ m.cos(theta), 0, m.sin(theta)],
+  return np.matrix([[ math.cos(theta), 0, math.sin(theta)],
                     [ 0           , 1, 0           ],
-                    [-m.sin(theta), 0, m.cos(theta)]])
+                    [-math.sin(theta), 0, math.cos(theta)]])
   
 def Rz(theta):
-  return np.matrix([[ m.cos(theta), -m.sin(theta), 0 ],
-                    [ m.sin(theta), m.cos(theta) , 0 ],
+  return np.matrix([[ math.cos(theta), -math.sin(theta), 0 ],
+                    [ math.sin(theta), math.cos(theta) , 0 ],
                     [ 0           , 0            , 1 ]])
   
 def get_rotation_matrix(angle:float=0, axis:str="x"):
@@ -23,6 +23,30 @@ def get_rotation_matrix(angle:float=0, axis:str="x"):
         return Ry(angle)
     elif axis == "z":
         return Rz(angle)
+
+ 
+def euler_from_quaternion(quat):
+        """
+        Convert a quaternion into euler angles (roll, pitch, yaw)
+        roll is rotation around x in radians (counterclockwise)
+        pitch is rotation around y in radians (counterclockwise)
+        yaw is rotation around z in radians (counterclockwise)
+        """
+        x, y, z, w = quat
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll_x = math.atan2(t0, t1)
+     
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch_y = math.asin(t2)
+     
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw_z = math.atan2(t3, t4)
+     
+        return [roll_x, pitch_y, yaw_z] # in radians
 
 def interpolate(t:float):
     # smooth 0-1 interval curve
