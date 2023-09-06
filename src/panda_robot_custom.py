a94486f00909f539d88a98d48645020a16c58561
BlazGo <bg2286@student.uni-lj.si>
2023-09-06 06:19:22 +0200

    restarted ros files

--- src/panda_robot_custom.py
+++ src/panda_robot_custom.py
@@ -12,7 +12,6 @@ import numpy as np
 import my_log
 import tools
 
-log = my_log.logger()
 
 class PandaRobotCustom:
     # topics
@@ -24,15 +23,18 @@ class PandaRobotCustom:
     list_controllers_topic = "controller_manager/list_controllers"
     cartesian_pose_topic = "/cartesian_impedance_example_controller/equilibrium_pose"
 
-    BASE_LINK:str = "panda_link0"
-    EE_frame:str = "panda_EE"
+    BASE_LINK: str = "panda_link0"
+    EE_frame: str = "qbhand_flange1"
+
+    Q_INIT: list = [0, -0.785398163, 0, -2.35619449, 0, 1.57079632679, 0.785398163397]
 
     # controller strategies
     STRICT: int = 2
     BEST_EFFORT: int = 1
 
-    def __init__(self):
-        log.info("Initializing PandaRobot class")
+    def __init__(self, log_level=my_log.INFO):
+        self.log = my_log.logger(log_level)
+        self.log.info("Initializing PandaRobot class")
 
         self.compliance_client = Client(self.compliance_topic, timeout=1)
         self.state_listener = rospy.Subscriber(self.franka_state_topic,
@@ -40,53 +42,33 @@ class PandaRobotCustom:
                                                callback=self.franka_state_callback,
                                                queue_size=1,)
 
-        self.cartesian_goal_publisher = rospy.Publisher(
-            self.cartesian_pose_topic, PoseStamped, queue_size=1)
+        self.cartesian_goal_publisher = rospy.Publisher(self.cartesian_pose_topic, PoseStamped, queue_size=1)
         self.tf_listener = tf.TransformListener()
 
         # Services
-        self.set_EE_frame_client = rospy.ServiceProxy(
-            self.set_EE_frame_topic, SetEEFrame)
-        self.set_EE_load_client = rospy.ServiceProxy(
-            self.set_EE_load_topic, SetLoad)
-        self.switch_controller_proxy = rospy.ServiceProxy(
-            self.switch_controller_topic, SwitchController)
-        self.list_controllers_proxy = rospy.ServiceProxy(
-            self.list_controllers_topic, ListControllers)
+        self.set_EE_frame_client = rospy.ServiceProxy(self.set_EE_frame_topic, SetEEFrame)
+        self.set_EE_load_client = rospy.ServiceProxy(self.set_EE_load_topic, SetLoad)
+        self.switch_controller_proxy = rospy.ServiceProxy(self.switch_controller_topic, SwitchController)
+        self.list_controllers_proxy = rospy.ServiceProxy(self.list_controllers_topic, ListControllers)
         rospy.sleep(0.5)
         self.init_robot()
 
-        log.info(f"Panda initialization {my_log.GREEN}DONE")
+        self.log.info(f"Panda initialization {my_log.GREEN}DONE")
 
     def init_robot(self):
         self.used_controllers:list = [cnt.name for cnt in self.list_controllers()]
         
-        # Stiffness setup
-        log.info(f"Setting stiffness")
-        log.info(f"Old stiffness: {self.get_stiffness()}")
-        self.set_stiffness(trans=400.0, rot=20.0, null=1.0)
-        log.info(f"New stiffness: {self.get_stiffness()}")
-        # Wait for he robot to stop moving
-        rospy.sleep(1.5)
-        
-        # Get init pose
-        pose = self.get_cart_pose()
-        self.q_init = self.get_state().q
-        self.cart_init = pose
-        log.info(f"Trans:{pose[0]}, Rot: {euler_from_quaternion(pose[1])}")
-        log.info(f"q_states: {self.q_init}")
-
         # EE frame transformation construction
         T_trans = np.eye(4)
-        T_trans[0:3, 3] = np.array([0.1, -0.0, 0.0]).T
+        T_trans[0:3, 3] = np.array([0.0, -0.0, 0.2]).T
 
         T_rot = np.eye(4)
-        T_rot[0:3, 0:3] = tools.get_rotation_matrix(angle=np.deg2rad(-45), axis="z")
+        T_rot[0:3, 0:3] = tools.get_rotation_matrix(angle=np.deg2rad(45), axis="z")
 
         T_new = np.matmul(T_rot, T_trans)
-        T_new = np.reshape(T_new, (16), order = 'F') # msg formaat
+        T_new = np.reshape(T_new, (16), order = 'C') # msg formaat
 
-        log.debug(f"{T_new}")
+        self.log.debug(f"{T_new}")
         
         # EE frame parameters
         self.tool_mass = 0.0
@@ -96,19 +78,24 @@ class PandaRobotCustom:
                              0.0,      0.0, 0.0,
                              0.0,      0.0,     0.0]
         
-        self.switch_controller(stop_controllers=self.used_controllers)
+        response = self.switch_controller(stop_controllers=self.used_controllers)
+        self.log.debug(f"Response switch controllers: {response}")
         response = self.set_EE_load(self.tool_mass,
                                     self.F_x_center_tool,
                                     self.tool_inertia)
-        log.info(f"Response set load: {response}")
+        self.log.debug(f"Response set EE load: {response}")
         response = self.set_NE_T_EE_transform(NE_T_EE=T_new)
-        log.info(f"Response set EE: {response}")
-        self.switch_controller(start_controllers=self.used_controllers)
+        self.log.debug(f"Response set EE trans: {response}")
+        response = self.switch_controller(start_controllers=self.used_controllers)
+        self.log.debug(f"Response switch controllers: {response}")
+
+        # Stiffness setup
+        self.set_stiffness(trans=400.0, rot=20.0)
+        self.log.debug(f"Stiffness: {self.get_stiffness()}")
 
     def franka_state_callback(self, msg) -> None:
         # Save the state
         self.state = msg
-        log.debug("Callback recieved")
 
     def get_state(self) -> FrankaState:
         # Return the robot state msg
@@ -119,10 +106,9 @@ class PandaRobotCustom:
         if self.tf_listener.frameExists(frame0) and self.tf_listener.frameExists(frame1):
             t = self.tf_listener.getLatestCommonTime(frame0, frame1)
             trans, rot = self.tf_listener.lookupTransform(frame0, frame1, t)
-            #log.info(f"Trans: {trans}, Rot: {rot}")
             return trans, rot
         else:
-            log.error(f"Transform could not be fetched ('{frame0}' to '{frame1}')")
+            self.log.error(f"Transform could not be fetched ('{frame0}' to '{frame1}')")
             return None
 
     def set_stiffness(self, trans: float = None, rot: float = None, null: float = None):
@@ -142,7 +128,6 @@ class PandaRobotCustom:
             response = self.compliance_client.update_configuration(change)
             rospy.sleep(0.25)
             responses.append(response)
-        rospy.sleep(0.5)
         return responses
 
     def get_stiffness(self):
@@ -169,9 +154,9 @@ class PandaRobotCustom:
         return response
 
     def switch_controller(self, start_controllers: list = [""], stop_controllers: list = [""], strictness: int = BEST_EFFORT, start_asap: bool = False) -> SwitchControllerResponse:
-        log.debug("Switching controllers")            
-        log.debug(f"Starting: {start_controllers}")
-        log.debug(f"Stopping: {stop_controllers}")
+        self.log.debug("Switching controllers")            
+        self.log.debug(f"Starting: {start_controllers}")
+        self.log.debug(f"Stopping: {stop_controllers}")
         
         msg = SwitchControllerRequest()
         msg.start_controllers = start_controllers
@@ -181,7 +166,7 @@ class PandaRobotCustom:
         msg.timeout = 1.0
 
         response = self.switch_controller_proxy.call(msg)
-        rospy.sleep(1.5)
+        rospy.sleep(1.0)
         return response
 
     def list_controllers(self) -> ListControllersResponse:
@@ -211,6 +196,7 @@ class PandaRobotCustom:
 
 
 if __name__ == "__main__":
+
     rospy.init_node("panda_custom", anonymous=True)
-    robot = PandaRobotCustom()
-    trans, rot = robot.get_cart_pose()
+    robot = PandaRobotCustom(log_level=my_log.DEBUG)
+    #trans, rot = robot.get_cart_pose()
