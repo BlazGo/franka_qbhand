b3a66ebb4ec57943efabee41ef11a4b7c458dffe
BlazGo <bg2286@student.uni-lj.si>
2023-10-09 19:00:07 +0200

    code match to env

--- src/result_vis.py
+++ src/result_vis.py
@@ -12,22 +12,22 @@ if __name__ == "__main__":
     plt.rcParams["figure.figsize"] = [7.50, 3.50]
     #plt.rcParams["figure.autolayout"] = True    
     
-    OBSERVATION_SPACE_SHAPE = [7, 7, 5]
+    OBSERVATION_SPACE_SHAPE = [9, 9, 7]
     
-    x_range = 0.05
-    y_range = 0.05
-    r_range = 30
+    x_range=[0.05, -0.05]
+    y_range=[0.05, -0.05]
+    r_range=[0, -60]
     
-    x = np.linspace(-x_range, x_range, OBSERVATION_SPACE_SHAPE[0])
-    y = np.linspace(-y_range, y_range, OBSERVATION_SPACE_SHAPE[1])
-    r = np.linspace(-r_range, r_range, OBSERVATION_SPACE_SHAPE[2])
+    x = np.linspace(x_range[0], x_range[1], OBSERVATION_SPACE_SHAPE[0])
+    y = np.linspace(y_range[0], y_range[1], OBSERVATION_SPACE_SHAPE[1])
+    r = np.linspace(r_range[0], r_range[1], OBSERVATION_SPACE_SHAPE[2])
 
     xv, yv = np.meshgrid(x, y)
 
     fig = plt.figure()
     ax = Axes3D(fig)
 
-    reward_layer = reward_table[:,:,2]
+    reward_layer = reward_table[:,:,5]
     scplot = ax.plot_surface(xv, yv, reward_layer, cmap="YlGnBu")
     # scplot = ax.scatter(xv, yv, r[2], c=reward_layer.flatten())
     plt.colorbar(scplot)
